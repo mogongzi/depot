e2c3782691bd62542e9bbfe59502eebf1aec4316
Ryan Ren <ryan.ren@sap.com>
2019-02-25 17:07:50 +0800

    fix controller test failure

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,11 @@ require 'active_model/serializers/xml'
 class Order < ApplicationRecord
   include ActiveModel::Serializers::Xml
 
-  enum pay_type: [:check, :credit_card, :purchase_order]
+  PAYMENT_TYPES = ['Check', 'Credit card', 'Purchase order']
 
   has_many :line_items, dependent: :destroy
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+  validates :pay_type, inclusion: PAYMENT_TYPES
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
