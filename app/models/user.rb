0ddf7d8d9d562ed78a74db42b2b1cbdceb9e470c
Ryan Ren <ryan.ren@sap.com>
2019-06-09 21:30:47 +0800

    some minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,8 @@ class User < ApplicationRecord
   end
 
   private
+
   def ensure_an_admin_remains
-    if User.count.zero?
-      raise Error.new "Can't delete last user"
-    end
+    raise Error.new, "Can't delete last user" if User.count.zero?
   end
 end
