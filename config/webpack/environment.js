0f59a6cc9b8509bdf7848187e5453bd08a2acb76
mogongzi <superrenyuan@hotmail.com>
2020-01-17 21:26:18 +0800

    add webpack-bundle-analyzer into this project

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker');
 
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+environment.plugins.append(
+    'BundleAnalyzer',
+    new BundleAnalyzerPlugin()
+);
+
 module.exports = environment;
