723fb6ea60d5de23426e9749ee3d81aa41e020b6
mogongzi <superrenyuan@hotmail.com>
2020-01-27 13:09:32 +0800

    adjust webpack-bundle-analyzer for rake test impaction

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker');
 
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-environment.plugins.append(
-    'BundleAnalyzer',
-    new BundleAnalyzerPlugin()
-);
-
 module.exports = environment;
