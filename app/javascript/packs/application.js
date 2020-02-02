fd70d720b9f2162fab9ecff84371e58048e1f356
mogongzi <superrenyuan@hotmail.com>
2020-02-02 22:57:37 +0800

    re-import scss file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,15 +4,6 @@ require("jquery");
 // require("@rails/activestorage").start();
 // require("channels");
 // import all scss files
-import "../scss/application.scss"
-import "../scss/admin.scss"
-import "../scss/carts.scss"
-import "../scss/line_items.scss"
-import "../scss/orders.scss"
-import "../scss/products.scss"
-import "../scss/scaffolds.scss"
-import "../scss/sessions.scss"
-import "../scss/store.scss"
-import "../scss/users.scss"
+require.context('../scss', true);
 // import all images
 require.context('../images', true);
